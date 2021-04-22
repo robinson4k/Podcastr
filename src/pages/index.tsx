94c668817dc36513d724cf562c47623084991446
Robinson Silva <robinson.silva@piauiconectado.com.br>
2021-04-22 18:48:42 -0400

    modelo SPA

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react"
+
 export default function Home() {
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, [])
   return (
     <h1>aqui</h1>
   )
