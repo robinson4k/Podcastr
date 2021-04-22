8bcd20946e75364efd9ef0199b0d243d114579cc
Robinson Silva <robinson.silva@piauiconectado.com.br>
2021-04-22 19:05:22 -0400

    modelo SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,14 @@ export default function Home(props) {
   )
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const response = await fetch('http://localhost:3333/episodes')
   const data = await response.json()
     
   return {
     props: {
       episodes: data,
-    }
+    },
+    revalidate: 60 * 60 * 8,
   }
 }
