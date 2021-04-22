6476c356fdd784c0b40c6a790f65dc1427cd075e
Robinson Silva <robinson.silva@piauiconectado.com.br>
2021-04-22 18:56:14 -0400

    modelo SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
-import { useEffect } from "react"
-
-export default function Home() {
-  useEffect(() => {
-    fetch('http://localhost:3333/episodes')
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }, [])
+export default function Home(props) {
   return (
-    <h1>aqui</h1>
+    <div>
+      <h1>aqui</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+    
+  return {
+    props: {
+      episodes: data,
+    }
+  }
 }
